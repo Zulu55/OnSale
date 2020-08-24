eb20ba6e2d99635eab3c587c79eef359175a12b8
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2020-08-24 13:11:09 -0500

    Change password

--- OnSale.Web/Controllers/AccountController.cs
+++ OnSale.Web/Controllers/AccountController.cs
@@ -228,5 +228,37 @@ namespace OnSale.Web.Controllers
             model.Departments = _combosHelper.GetComboDepartments(model.CityId);
             return View(model);
         }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserAsync(User.Identity.Name);
+                if (user != null)
+                {
+                    var result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ChangeUser");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User no found.");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
