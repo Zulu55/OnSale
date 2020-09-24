438b51f5d0bc9303e4852e9d98f8699cd5e54291
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2020-09-24 17:56:21 -0500

    Add random user

---
+++ OnSale.Web/Migrations/20200924223943_RandomUsers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnSale.Web.Migrations
+{
+    public partial class RandomUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Logitude",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Logitude",
+                table: "AspNetUsers");
+        }
+    }
+}
